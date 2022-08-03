219adf1cea0f174ae1bd4117a4c55a11f01e77f0
matdsk <matdsk.work@gmail.com>
2022-08-03 18:48:45 +0200

    create heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Iron Man' },
+  { id: 13, name: 'Thor' },
+  { id: 14, name: 'Hulk' },
+  { id: 15, name: 'Natasha' },
+  { id: 16, name: 'Vanda' },
+  { id: 17, name: 'Dr. Strange' },
+  { id: 18, name: 'Capitan Marvel' },
+  { id: 19, name: 'Antman' },
+  { id: 20, name: 'Spider-Man' }
+];
